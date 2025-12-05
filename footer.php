4e9de5aeef028371e27886fc421c1407f44f4b64
Donito <donovanandy4@gmail.com>
2025-12-05 16:52:38 +0100

    message

---
+++ footer.php
@@ -0,0 +1,3 @@
+<?php
+$pdo = new PDO("mysql:host=localhost", "root", "");
+$query = $pdo ->query("")
--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-alert ("Hello World");
-console.log("NSNIVVQPISCQN");
+alert ("Coucou Clarisse ça va ?");
+document.addEventListener("DOMContentLoaded",()=>{
+    const form = document.getElementById("contactForm");
+    const message = document.getElementById("message");
+    form.addEventListener("submit", (e)=>{ 
+        e.preventDefault();
+        const nom = form.nom.value.trim();
+        if (nom!==""&& ElementInternals.includes("@")){
+            message.textContent="Votre demande est bien envoyée !";
+            message.style.color = "Black";
+        }else{ 
+            message.textContent="Vos informations ne sont pas correctes"
+        } message.style.display="block"
+    });
+});
